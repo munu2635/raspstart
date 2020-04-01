f97f4fe253ca196295ee4a406fa967c9b54d8753
laply <lapply2635@gmail.com>
2020-04-01 10:27:04 +0900

    update 0401 connect output

--- sensingData3/sensor.py
+++ sensingData3/sensor.py
@@ -4,13 +4,9 @@ import mqtt
 
 class Sensor :
 	all_pin = [22, 25, 24, 23, 27, 16, 26]
-	# dht11_pin = 22 / fire_pin = 25 / led_red_pin = 24 / led_green_pin = 23
-	# shock_pin = 27 / ir_sensor_pin = 16 / button_pin = 26
 
 	# dev server IP 
-	# ipPort = ["124.139.136.86", "1883"]  
-	# test server IP
-	ipPort = ["115.20.144.97", "11183"]
+	ipPort = ["124.139.136.86", "1883"]  
 
 
 	sending = mqtt.Connect(ipPort)
