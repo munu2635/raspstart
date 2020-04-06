e4fd777e293908bbac0bd03c61bdc58c0ebba695
laply <lapply2635@gmail.com>
2020-04-06 14:50:26 +0900

    update0406

--- mjpg.sh
+++ mjpg.sh
@@ -1,4 +1,4 @@
-export STREAMER_PATH=home/pi/streamer/mjpg-streamer/mjpg-streamer-experimental
+export STREAMER_PATH=home/pi/raspstart/mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=$STREAMER_PATH
 $STREAMER_PATH/mjpg_streamer -i "input_raspicam.so" -o "output_http.so -p 8891 -w $STREAMER_PATH/www/"
 
