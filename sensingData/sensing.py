a5c2e4807d3a2274fd507f94458cf2e76bed1e55
Laply <lapply2635@gmail.com>
2020-04-10 17:16:34 +0900

    Update0410

--- sensingData/sensing.py
+++ sensingData/sensing.py
@@ -30,21 +30,21 @@ class Sensing :
 		self.receive = receive.Receive([self.sensorTimerControl, self.sensorDetectControl], self.cameraIpPort, self.topic)
 
 	def setInstance(self, GPIO):
-		if self.sensordata[1][0] :
+		if self.useSensor[0] :
 			self.sensorTimerControl.append(DHT11.Control(self.all_pin[0], GPIO, self.topic,[0 ,1])) 
-		if self.sensordata[1][1] :
+		if self.useSensor[1] :
 			self.sensorDetectControl.append(Fire.Control(self.all_pin[1], GPIO, self.topic, 0))
-		if self.sensordata[1][2] :
+		if self.useSensor[2] :
 			self.sensorDetectControl.append(Shock.Control(self.all_pin[4], GPIO, self.topic, 1)) 
-		if self.sensordata[1][3] :
+		if self.useSensor[3] :
 			self.sensorDetectControl.append(IR.Control(self.all_pin[5], GPIO, self.topic, 2))
-		if self.sensordata[1][4] :
+		if self.useSensor[4] :
 			print("set gas sensor")
-		if self.sensordata[1][5] :
+		if self.useSensor[5] :
 			print("set cds sensor")
-		if self.sensordata[1][6] :
+		if self.useSensor[6] :
 			self.button_instance = Button.Control(self.all_pin[6], GPIO, self.topic)
-		if self.sensordata[1][7] :
+		if self.useSensor[7] :
 			self.led_instance = LED.LED(self.all_pin[3], self.all_pin[2], GPIO)
 			self.led_instance.write(1)
 
@@ -59,8 +59,8 @@ class Sensing :
 
 		for i in self.sensorDetectControl :
 			data = i.check()
-			if data == True and self.sensordata[1][7] :
+			if data == True and self.useSensor[7] :
 				self.led_instance.write(0)
 
-		if self.sensordata[1][6] and self.button_instance.clearButton(self.sensorDetectControl):
+		if self.useSensor[6] and self.button_instance.clearButton(self.sensorDetectControl):
 			self.led_instance.write(1)
