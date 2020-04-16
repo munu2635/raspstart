91fb8864d04decd8d670251340862ffdc33e4984
Laply <lapply2635@gmail.com>
2020-04-16 17:54:01 +0900

    update0416(motorSG90)

--- sensingData/setting.py
+++ sensingData/setting.py
@@ -9,9 +9,9 @@ cameraLocalport = "8891"
 useSensor = [True, True, True, True, False, False, True, True, True ]
  # dht11 / fire / shock / ir / gas / cds / button / led 
 
-all_pin = [22, 25, 24, 23, 27, 16, 26, 6, 5, 20, 21]
+all_pin = [22, 25, 24, 23, 27, 16, 26, 6, 5, 17, 21]
 	# dht11_pin = 22 / fire_pin = 25 / led_red_pin = 24 / led_green_pin = 23
 	# shock_pin = 27 / ir_sensor_pin = 16 / button_pin = 26 / gas_pin = 6
-	# cds_pin = 5 / motor_pin_1 = 20 / motor_pin_2 = 21
+	# cds_pin = 5 / motor_pin_1 = 17 / motor_pin_2 = 21
 
 sensordata = [all_pin, useSensor]
