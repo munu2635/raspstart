c234e5798b6ede887b8806dc03eb2be09fb56f1a
Laply <lapply2635@gmail.com>
2020-04-16 18:15:12 +0900

    update0416(motorSG90)

--- sensingData/SG90.py
+++ sensingData/SG90.py
@@ -14,8 +14,8 @@ class SG90:
             self.GPIO.setup(self.pin_motor1, self.GPIO.OUT)
             self.GPIO.setup(self.pin_motor2, self.GPIO.OUT)
 
-            self.p1 = self.GPIO.PWM(self.pin_motor1, 10)
-            self.p2 = self.GPIO.PWM(self.pin_motor2, 10)
+            self.p1 = self.GPIO.PWM(self.pin_motor1, 20)
+            self.p2 = self.GPIO.PWM(self.pin_motor2, 20)
 
             self.p1.start(0)
             self.p2.start(0)
